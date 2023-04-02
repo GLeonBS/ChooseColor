7571e05266072dad13a1c9d6c3397750bd11cac1
GLeonBS <gabriel.leonbs@gmail.com>
2023-04-02 17:14:00 -0300

    Tentanto usar conta gotas

--- src/application/buscaGato.ts
+++ src/application/buscaGato.ts
@@ -1,36 +1,45 @@
 export const buscarGato = () => {
-    const baseUrl = 'https://api.thecatapi.com/v1/breeds/';
+    // const baseUrl = 'https://api.thecatapi.com/v1/breeds/';
 
     const searchInput = document.querySelector<HTMLInputElement>('.search-input')!
     const searchButton = document.querySelector<HTMLButtonElement>('.search-button')!
     const container = document.querySelector('.gato')!
+    
+    
+
+    searchButton.addEventListener('click', event => {
+        const dropper  =  new EyeDropper()
+        let output = document.querySelector<HTMLDivElement>('.quadrado')!
+        dropper.open().then((result: any) => {
+            console.log(result.sRGBHex);
+        })
+    })
     //criar constante que busca a mensagem de erro no HTML
 
-    let nomeGato
-    let gato
-    let card
+    // let nomeGato
+    // let gato
+    // let card
 
-    fetch(baseUrl)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err))
+    // fetch(baseUrl)
+    //     .then(response => response.json())
+    //     .then(data => console.log(data))
+    //     .catch(err => console.error(err))
 
-    function requestCat(url : string, name: string){
-        fetch(url + name)
-            .then(response => response.json())
-            .then(data => {
-                gato = data
-                console.log(gato);
-            })
-            .catch(err => console.error(err))
+    // function requestCat(url : string, name: string){
+    //     fetch(url + name)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             gato = data
+    //             console.log(gato);
+    //         })
+    //         .catch(err => console.error(err))
             
-    }
+    // }
     
-    searchButton.addEventListener('click', event => {
-        event.preventDefault()
-        nomeGato = searchInput.value.toLowerCase()
-        requestCat(baseUrl, nomeGato)
-    })
-    
+    // searchButton.addEventListener('click', event => {
+    //     event.preventDefault()
+    //     nomeGato = searchInput.value.toLowerCase()
+    //     requestCat(baseUrl, nomeGato)
+    // })
     
 }
