dbc890563e1db676c9e457f1a864136410604d56
GLeonBS <gabriel.leonbs@gmail.com>
2023-04-03 22:02:17 -0300

    Ententendo EyeDropper

--- src/application/queCor.ts
+++ src/application/queCor.ts
@@ -6,10 +6,10 @@ export const chooseColor = () => {
     const errorMessage = document.querySelector<HTMLSpanElement>('.error')!
     const output = document.querySelector<HTMLDivElement>('.quadrado')!
     
-    
+    const eyeDropper = new EyeDropper();
 
     searchButton.addEventListener('click', event => {
-        const dropper  =  useEyeDropper()
+        const dropper  =  new (window as any).EyeDropper()
         dropper.open().then((result: any) => {
             console.log(result.sRGBHex);
         })
