f38d0f101cb19e3b4dcb09b41d94abfc572afdb1
GLeonBS <gabriel.leonbs@gmail.com>
2023-04-04 09:56:53 -0300

    Complementando HTML e CSS

--- src/application/queCor.ts
+++ src/application/queCor.ts
@@ -3,14 +3,18 @@ export const chooseColor = () => {
     const chooseButton = document.querySelector<HTMLButtonElement>('.choose-button')!
     const errorMessage = document.querySelector<HTMLSpanElement>('.error')!
     const output = document.querySelector<HTMLDivElement>('.cor')!
+    const rgb = document.querySelector<HTMLSpanElement>('.rgb')!
+    const corTitle = document.querySelector<HTMLTitleElement>('.cor-title')!
+    const ultimaCor = localStorage.getItem('corSelecionada')!
+    console.log(corTitle);
 
-    console.log(chooseButton);
-    console.log(errorMessage);
-    console.log(output);
-    
-    
-    
-
+    if(!localStorage.getItem('corSelecionada')){
+        corTitle.innerText = "Ainda não tem nenhuma cor selecionada :/"
+      }else {
+        corTitle.innerText = "Última cor selecionada:"
+        output.style.backgroundColor = ultimaCor
+        rgb.innerText = ultimaCor
+      }
 
     chooseButton.addEventListener('click', ev => {
 
@@ -20,25 +24,15 @@ export const chooseColor = () => {
         dropper
             .open()
             .then((result) => {
-                console.log(result.sRGBHex);
+                output.style.backgroundColor = result.sRGBHex
+                localStorage.setItem('corSelecionada', result.sRGBHex)
+                rgb.innerText = result.sRGBHex
+                corTitle.innerText = "Última cor selecionada:"
             })
             .catch((e) => {
                 errorMessage.style.display = 'block'
                 errorMessage.innerText = " ", e
             });
     })
-
-    // chooseButton.onclick = () => {
-    //     const eyeDropper = new EyeDropper();
-
-    //     eyeDropper
-    //         .open()
-    //         .then((result) => {
-    //             console.log(result.sRGBHex);
-    //         })
-    //         .catch((e) => {
-    //             errorMessage.style.display = 'block'
-    //             errorMessage.innerText = " ", e
-    //         });
-    // }
+    
 }
